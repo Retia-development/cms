3198118d21166d46b473e755490cb1d4ade395bc
Thomas Farla <thomas-farla@hotmail.com>
2014-02-02 15:52:45 +0100

    Assign model class to loader when it is loaded

--- core/loader.php
+++ core/loader.php
@@ -1,14 +1,27 @@
 <?php
 namespace Core;
+require_once('core/exceptions/class_not_found.php');
 
 class Loader {
-    public function __construct() {
+    public $model;
 
+    public function __construct() {
+        $this->model = new \StdClass();
     }
 
-    public function model($model_name) {
+    public function model($model_name, $alias=NULL) {
         $path_to_model = ENVIRONMENT_MODELS . "/$model_name.php";
+        if (!file_exists($path_to_model)) {
+            throw new \Core\Exceptions\ClassNotFound($model_name, 'model');
+        }
+
         require_once($path_to_model);
-        return new $model_name();
+
+        if (!class_exists($model_name)) {
+            throw new \Core\Exceptions\ClassNotFound($model_name, 'model');
+        }
+
+        $reffer_as = is_null($alias) ? ucfirst($model_name) : $alias;
+        return $this->model->$reffer_as = new $model_name();
     }
 }
--- tests/loader.php
+++ tests/loader.php
@@ -1,4 +1,5 @@
 <?php
+require_once('core/exceptions/class_not_found.php');
 require_once('core/loader.php');
 use Core\Loader as Loader;
 
@@ -7,5 +8,29 @@ class LoaderTest extends PHPUnit_Framework_TestCase {
         $loader = new Loader();
         $model = $loader->model('MyModel');
         $this->assertEquals($model->test(), 'test');
+    }
+
+    public function test_load_invalid_model_file() {
+        $this->setExpectedException('\Core\Exceptions\ClassNotFound');
+        $loader = new Loader();
+        $loader->model('InValidModel');
+    }
+
+    public function test_load_invalid_model_class() {
+        $this->setExpectedException('\Core\Exceptions\ClassNotFound');
+        $loader = new Loader();
+        $loader->model('ModelWithDifferentNameThanFile');
+    }
+
+    public function test_save_model_to_loader() {
+        $loader = new Loader();
+        $loader->model('myModel');
+        $this->assertTrue(isset($loader->model->MyModel));
+    }
+
+    public function test_save_model_with_alias() {
+        $loader = new Loader();
+        $loader->model('myModel', 'myLittleModel');
+        $this->assertTrue(isset($loader->model->myLittleModel));
     }
 }
