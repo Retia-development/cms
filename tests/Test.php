10fbcaee3bafdd4b4ca1bd863f42009da020734f
Thomas Farla <thomas-farla@hotmail.com>
2014-02-02 11:27:46 +0100

    Find tests dynamicly

--- tests/Test.php
+++ tests/Test.php
@@ -1,10 +1,20 @@
-<?php 
+<?php
 define('ENVIRONMENT', 'test');
 define('ENVIRONMENT_CONTROLLERS', 'tests/controllers/');
 
-require('case.php');
-require('routes.php');
-require('exceptions.php');
-$c = new CaseTest();
-$routes = new RoutesTest();
-$exceptions = new ExceptionsTest();
+$test_classess = [];
+foreach (glob('tests/*.php') as $file) {
+    $filename = strtolower(pathinfo($file, PATHINFO_FILENAME));
+    if ($filename == 'test') {
+        continue;
+    }
+
+    include($file);
+    $class_name = ucfirst("{$filename}Test");
+    if (!class_exists($class_name)) {
+        echo "WARNING: Can't find class $class_name in $file \n";
+        continue;
+    }
+
+    $test_classess[] = new $class_name();
+}
