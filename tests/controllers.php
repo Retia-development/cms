92e1c12addc8fcf698a15d90ebb2622d1732cac6
Tom Schillemans <tomschillemans@gmail.com>
2014-02-02 17:56:38 +0100

    Added a test to check if BaseController exists Added BaseController class in core/controllers

--- tests/controllers.php
+++ tests/controllers.php
@@ -1,6 +1,9 @@
 <?php
+require_once('core/controllers/base_controller.php');
+
 class ControllerTest extends PHPUnit_Framework_TestCase {
     public function test_exists_base_controller() {
+        $this->assertTrue(class_exists('BaseController'));
 
     }
 
