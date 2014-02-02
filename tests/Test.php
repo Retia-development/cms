537bb18781ee3653514ff171f7ac0090c76eeeed
Tom Schillemans <tomschillemans@gmail.com>
2014-02-02 10:47:34 +0100

    Added controllers test to Test.php and made a start on controllers test
    Signed-off-by: Tom Schillemans <tomschillemans@gmail.com>

--- tests/Test.php
+++ tests/Test.php
@@ -1,5 +1,7 @@
-<?php 
+<?php
 require('case.php');
 require('routes.php');
+require('controllers.php');
 $c = new CaseTest();
-$routes = new RoutesTest();
+$routes = new RoutesTest
+$controllers = new ControllerTest;
---
+++ tests/controllers.php
@@ -0,0 +1,8 @@
+<?php
+class ControllerTest extends PHPUnit_Framework_TestCase {
+    public function test_exists_base_controller() {
+
+    }
+
+
+}
